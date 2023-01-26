0c35cf72c78f5fa597e09d25c3cec4a39d3db71d
Sallai András <andrewteki@gmail.com>
2023-01-26 20:12:59 +0100

    JavaScript kész

--- src/app.js
+++ src/app.js
@@ -7,3 +7,33 @@
 * Github: https://github.com/andteki/
 * Licenc: GNU GPL
 */
+
+const asideElem = document.querySelector("#aside");
+const bsideElem = document.querySelector("#bside");
+const csideElem = document.querySelector("#cside");
+const dsideElem = document.querySelector("#dside");
+const calcButton = document.querySelector("#calcButton");
+const areaElem = document.querySelector("#area");
+
+calcButton.addEventListener('click', () => {
+    //a=13, b=4, c=7, d=4
+//    console.log('műkdik') 
+   let aside = Number(asideElem.value);
+   let bside = Number(bsideElem.value);
+   let cside = Number(csideElem.value);
+   let dside = Number(dsideElem.value);
+//    console.log(aside)
+   let area = calcArea(aside, bside, cside, dside);
+   areaElem.value = area;
+});
+
+function calcArea(a, b, c, d) {
+    let terulet = ( (a + c)/(4*(a-c)) ) 
+    * Math.sqrt(
+        (a+b-c+d)*
+        (a-b-c+d)*
+        (a+b-c-d)*
+        (-a+b+c+d)
+    )
+    return terulet;
+}
